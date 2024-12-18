5f2b5026f4bea8571b6220bbf067c0d3d8828302
Pavel Ilchenko <arisesinmight@gmail.com>
2024-12-18 21:14:50 +0300

    fix lint

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,7 +9,6 @@ from flask import (Flask, flash, get_flashed_messages, redirect,
 from page_analyzer.urls_repository import UrlsRepository
 
 load_dotenv()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
